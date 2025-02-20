be7b8392ff3b8b0aacee1ce8ae1e97c198ac313b
Valentin Lab <valentin.lab@kalysto.org>
2025-02-20 20:46:52 +0100

    new: ``transaction.isReconversion`` property now return string state also
    If false, it is not a reconversion, if true it is a reconversion but wasn't
    yet acknowledged by the administrative backend. If it is a string, then it
    is the state as defined by the administrative backend.

--- src/transaction.ts
+++ src/transaction.ts
@@ -2,6 +2,18 @@ import { t } from '@lokavaluto/lokapi'
 import { BridgeObject, Transaction } from '@lokavaluto/lokapi/build/backend'
 
 
+export function isReconversion(jsonData, backend) {
+    if (jsonData.direction !== 1) return false
+    const recipient = jsonData['addr_to']
+    const safeWallet = backend.jsonData?.safe_wallet_recipient
+    if (!safeWallet) return false
+    return (
+        recipient ===
+            '0x' + safeWallet.monujo_backends[backend.internalId][0]
+    )
+}
+
+
 export class ComchainTransaction extends Transaction implements t.ITransaction {
 
     get amount () {
@@ -66,15 +78,13 @@ export class ComchainTransaction extends Transaction implements t.ITransaction {
     }
 
     get isReconversion () {
-        if (this.jsonData.comchain.direction !== 1) return false
-        const recipient = this.jsonData.comchain['addr_to']
-        const safeWallet = this.parent.parent.jsonData?.safe_wallet_recipient
-        if (!safeWallet) return false
-        let backend = this.parent.parent
-        return (
-            recipient ===
-            '0x' + safeWallet.monujo_backends[backend.internalId][0]
-        )
+        const backend = this.parent.parent
+        if (!isReconversion(this.jsonData.comchain, backend)) {
+            return false
+        }
+
+        const backendInternalId = backend.internalId.replace(":", "://")
+        return this.jsonData.odoo.reconversionStatusResolve[`${backendInternalId}/tx/${this.id}`] || true
     }
 
 }
